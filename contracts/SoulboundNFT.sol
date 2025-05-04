3c2b0824f468eb10cdc0281d78e9ba5dace19ecb
Vikalp Nag <vikalpnag@gmail.com>
2025-05-05 01:42:22 +0530

    declared Variables

---
+++ contracts/SoulboundNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SoulboundNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+}
