fcf2ee5381dd545dc7bfe282ee5b29d6e6d30724
Vikalp Nag <vikalpnag@gmail.com>
2025-05-05 19:33:37 +0530

    feat: enforce non-transferability of soulbound tokens via _beforeTokenTransfer

--- contracts/SoulboundNFT.sol
+++ contracts/SoulboundNFT.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract SoulboundNFT is ERC721, Ownable {
+contract SoulboundNFT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
 
     constructor(
@@ -20,5 +21,18 @@ contract SoulboundNFT is ERC721, Ownable {
         uint256 tokenId = nextTokenId++;
         _safeMint(user, tokenId);
         _setTokenURI(tokenId, metadataHash); //Can be IPFS URI or identity hash
+    }
+
+    ///@dev Override all transfer-related functions to prevent transfers
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        require(
+            from == address(0) || to == address(0),
+            "Soulbound:Tokens are non-transferable"
+        );
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
