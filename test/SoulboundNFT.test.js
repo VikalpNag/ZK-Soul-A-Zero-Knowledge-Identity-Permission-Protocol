ad22ce403625beb25a7dae5d6546f51e7e8e71e0
Vikalp Nag <vikalpnag@gmail.com>
2025-05-06 19:11:24 +0530

    initiated test

--- test/SoulboundNFT.test.js
+++ test/SoulboundNFT.test.js
@@ -4,4 +4,11 @@
 // describe("SoulboundNFT contract", function () {
 //   let owner, user1, user2, soulboundNFT;
 
+//   beforeEach(async () => {
+//     [owner, user1, user2] = await ethers.getSigners();
+
+//     const SoulboundNFT = await ethers.getContractFactory("SoulboundNFT");
+//     soulboundNFT = await SoulboundNFT.deploy("ZK-SoulID", "ZKSID");
+//     await soulboundNFT.waitForDeployment();
+//   });
 // });
