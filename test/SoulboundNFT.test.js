231f0672e5e8af750d430390f86d413582819141
Vikalp Nag <vikalpnag@gmail.com>
2025-05-08 01:22:11 +0530

    fix: test file issues

--- test/SoulboundNFT.test.js
+++ test/SoulboundNFT.test.js
@@ -1,64 +1,73 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const {
+  root: merkleRoot,
+  proof,
+  credentials,
+} = require("../scripts/generateMerkleTree.js");
 
-// describe("SoulboundNFT contract", function () {
-//   let owner, user1, user2, soulboundNFT;
+describe("SoulboundNFT contract", function () {
+  let owner, user1, user2, soulboundNFT;
 
-//   beforeEach(async () => {
-//     [owner, user1, user2] = await ethers.getSigners();
+  beforeEach(async () => {
+    [owner, user1, user2] = await ethers.getSigners();
 
-//     const SoulboundNFT = await ethers.getContractFactory("SoulboundNFT");
-//     soulboundNFT = await SoulboundNFT.deploy("ZK-SoulID", "ZKSID");
-//     await soulboundNFT.waitForDeployment();
-//   });
+    const SoulboundNFT = await ethers.getContractFactory("SoulboundNFT");
+    soulboundNFT = await SoulboundNFT.deploy("ZK-SoulID", "ZKSID");
+    await soulboundNFT.waitForDeployment();
+  });
 
-//   it("Should mint a soulbound token to user", async () => {
-//     const uri = "ipfs://QmTestMetadataHash";
-//     await soulboundNFT.connect(owner).mintSoulbound(user1.address, uri);
+  it("Should mint a soulbound token to user", async () => {
+    const uri = "ipfs://QmTestMetadataHash";
+    await soulboundNFT
+      .connect(owner)
+      .mintSoulbound(user1.address, uri, merkleRoot);
 
-//     expect(await soulboundNFT.ownerOf(0)).to.equal(user1.address);
-//     expect(await soulboundNFT.tokenURI(0)).to.equal(uri);
-//   });
+    expect(await soulboundNFT.ownerOf(0)).to.equal(user1.address);
+    expect(await soulboundNFT.tokenURI(0)).to.equal(uri);
+  });
 
-//   it("Should not allow transferFrom", async () => {
-//     await soulboundNFT
-//       .connect(owner)
-//       .mintSoulbound(user1.address, "ipfs://hash");
+  it("Should not allow transferFrom", async () => {
+    await soulboundNFT
+      .connect(owner)
+      .mintSoulbound(user1.address, "ipfs://hash", merkleRoot);
 
-//     await expect(
-//       soulboundNFT.connect(user1).transferFrom(user1.address, user2.address, 0)
-//     ).to.be.revertedWith("Soulbound:Transfer not allowed");
-//   });
+    await expect(
+      soulboundNFT.connect(user1).transferFrom(user1.address, user2.address, 0)
+    ).to.be.revertedWith("Soulbound:Transfer not allowed");
+  });
 
-//   it("Should not allow approve", async () => {
-//     await soulboundNFT
-//       .connect(owner)
-//       .mintSoulbound(user1.address, "ipfs://hash");
-//     await expect(
-//       soulboundNFT.connect(user1).approve(user2.address, 0)
-//     ).to.be.revertedWith("Soulbound:Approval not allowed");
-//   });
+  it("Should not allow approve", async () => {
+    await soulboundNFT
+      .connect(owner)
+      .mintSoulbound(user1.address, "ipfs://hash", merkleRoot);
+    await expect(
+      soulboundNFT.connect(user1).approve(user2.address, 0)
+    ).to.be.revertedWith("Soulbound:Approval not allowed");
+  });
 
-//   it("Should not allow setApprovalForAll", async () => {
-//     await expect(
-//       soulboundNFT.connect(user1).setApprovalForAll(user2.address, true)
-//     ).to.be.revertedWith("Soulbound:Approval not allowed");
-//   });
+  it("Should not allow setApprovalForAll", async () => {
+    await expect(
+      soulboundNFT.connect(user1).setApprovalForAll(user2.address, true)
+    ).to.be.revertedWith("Soulbound:Approval not allowed");
+  });
 
-//   it("Should not allow safeTransferFrom", async () => {
-//     await soulboundNFT
-//       .connect(owner)
-//       .mintSoulbound(user1.address, "ipfs://hash");
-//     await expect(
-//       soulboundNFT
-//         .connect(user1)
-//         .safeTransferFrom(user1.address, user2.address, 0)
-//     ).to.be.revertedWith("Soulbound: Transfer not allowed");
-//   });
+  it("Should not allow safeTransferFrom", async () => {
+    await soulboundNFT
+      .connect(owner)
+      .mintSoulbound(user1.address, "ipfs://hash", merkleRoot);
+    await expect(
+      soulboundNFT
+        .connect(user1)
+        .safeTransferFrom(user1.address, user2.address, 0)
+    ).to.be.revertedWith("Soulbound: Transfer not allowed");
+  });
 
-//   it("Only owner can mint", async () => {
-//     await expect(
-//       soulboundNFT.connect(user1).mintSoulbound(user2.address, "ipfs://hash")
-//     ).to.be.revertedWithCustomError(soulboundNFT, "OwnableUnauthorizedAccount");
-//   });
-// });
+  it("Only owner can mint", async () => {
+    await expect(
+      soulboundNFT
+        .connect(user1)
+        .mintSoulbound(user2.address, "ipfs://hash", merkleRoot)
+    ).to.be.revertedWithCustomError(soulboundNFT, "OwnableUnauthorizedAccount");
+  });
+});
