c949cdf701d04a7112755fbf0ab342eeed345c99
Vikalp Nag <vikalpnag@gmail.com>
2025-05-08 01:04:53 +0530

    add : merkleRoot in mintSoulbound

--- contracts/SoulboundNFT.sol
+++ contracts/SoulboundNFT.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract SoulboundNFT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
 
+    mapping(address => bytes32) identityRoots;
+
     constructor(
         string memory name,
         string memory symbol
@@ -16,11 +18,14 @@ contract SoulboundNFT is ERC721URIStorage, Ownable {
     ///@notice Mint a soulbound token to a user with Metadata (IPFS URI or Identity hash)
     function mintSoulbound(
         address user,
-        string memory metadataHash
+        string memory metadataHash,
+        bytes32 merkleRoot
     ) external onlyOwner {
         uint256 tokenId = nextTokenId++;
         _safeMint(user, tokenId);
         _setTokenURI(tokenId, metadataHash); //Can be IPFS URI or identity hash
+
+        identityRoots[user] = merkleRoot;
     }
 
     ///@dev Override transfer hook introduced in OZ v5
